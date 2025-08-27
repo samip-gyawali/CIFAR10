ae59d923be829c06ec536d4e6ac81945cf75f699
Samip Gyawali <samip@ba:38:82:78:6a:f3.home>
2025-08-27 17:25:30 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import keras
+import numpy as np
+from matplotlib import pyplot as plt
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu', input_shape=(32,32,3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(2,2), padding='same'),
+    keras.layers.Dropout(0.2),
+
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu', input_shape=(32,32,3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(2,2), padding='same'),
+    keras.layers.Dropout(0.2),
+
+    keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation='relu', input_shape=(32,32,3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(2,2), padding='same'),
+    keras.layers.Dropout(0.2),
+
+    keras.layers.Flatten(),
+    keras.layers.Dense(units=128, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(units=10,activation='softmax')]
+)
+
+model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+print(model.summary())
+exit()
+model.fit(x_train, y_train, epochs=10)
+model.evaluate(x_test, y_test)
